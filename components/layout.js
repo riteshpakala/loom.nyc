fb669ef64fdb0897be249db9d288b04d2a58d114
riteshpakala <rpakala@me.com>
2024-11-10 15:38:19 -0800

    winter

--- components/layout.js
+++ components/layout.js
@@ -127,7 +127,7 @@ export default function Layout({ children, home }) {
 
         <div className={`${styles.footer}`}>
           <span className={`${utilStyles.headingSm}`}> loom.nyc </span>
-          <span className={`${utilStyles.dandelionText} ${utilStyles.headingSm3}`}> YSL Summer '24 - SebastiAn </span>
+          <span className={`${utilStyles.dandelionText} ${utilStyles.headingSm3}`}> YSL Winter '24 - SebastiAn </span>
           
         </div>
         <div className={`${styles.player}`}>
